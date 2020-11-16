ecf5a3ebf852bb7e35a0fb92abd235e750d87073
Kevin <jeke20@gmail.com>
2020-11-15 20:16:26 -0500

    Cambio de Death Note por Love

--- src/GifExpertApp.js
+++ src/GifExpertApp.js
@@ -4,7 +4,7 @@ import { GifGrid } from './components/GifGrid';
 
 export const GifExpertApp = () => {
 
-    const [categories, setCategories] = useState(['Death Note']);
+    const [categories, setCategories] = useState(['Love']);
 
     return (
         <>
